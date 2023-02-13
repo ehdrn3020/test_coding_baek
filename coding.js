1beeadf4a46b8113db41ea29d47c04b4879786c2
ehdrn3020 <ehdrnsla3020@naver.com>
2023-02-14 06:34:51 +0900

    20230214 일곱난쟁이

--- coding.js
+++ coding.js
@@ -2,7 +2,7 @@
 
 const fs = require("fs");
 // beak에 제출시
-// let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+// let input = fs.readFileSync("./dev/stdin").toString().trim().split("\n");
 
 // VS CODE 에서
 let input = fs.readFileSync("./test_coding_baek/input.txt").toString().trim().split('\r\n');
--- input_guide.js
+++ input_guide.js
@@ -12,6 +12,9 @@ let input = fs.readFileSync('/dev/stdin').toString().split(' ');
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
+//정수 배열
+let input = fs.readFileSync('/dev/stdin').toString().split('\n').map(Number);
+
 
 [ readline module ]
 
